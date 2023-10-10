aee2781e451a85cfa3a587ea9b875ee9043e9f7f
Buğra Kağan <89995984+bklck06@users.noreply.github.com>
2023-10-10 15:09:10 +0300

    SocialEngineeringV1
    Social Engineering Forms For Instagram

---
+++ SocialEngineeringV1/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocialEngineeringV1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://help.instagram.com/contact/636276399721841");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://help.instagram.com/contact/383679321740945");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/help/instagram/contact/606967319425038");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://help.instagram.com/contact/723586364339719");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/help/instagram/547601325292351");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://help.instagram.com/contact/1652567838289083");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://help.instagram.com/contact/396169787183059");
+        }
+    }
+}
